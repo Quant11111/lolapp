b3eb07cf68964ef29b7ce4022e4c6180ed61b220
Quant11111 <lamagnere.quentin@gmail.com>
2024-08-27 12:28:03 +0200

    add next server config for produciton

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 
 const { withPlausibleProxy } = require("next-plausible");
 
-const nextConfig = withPlausibleProxy()({});
+const isProd = process.env.PROJECT_ENV === "production";
+
+const nextConfig = withPlausibleProxy()({
+  server: isProd
+    ? {
+        https: {
+          key: fs.readFileSync(
+            path.resolve(
+              __dirname,
+              "/etc/letsencrypt/live/padelu.orbweavers.com/privkey.pem",
+            ),
+          ),
+          cert: fs.readFileSync(
+            path.resolve(
+              __dirname,
+              "/etc/letsencrypt/live/padelu.orbweavers.com/fullchain.pem",
+            ),
+          ),
+        },
+      }
+    : {},
+});
 
 module.exports = nextConfig;
