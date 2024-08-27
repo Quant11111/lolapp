300175beef4179e62852a52437f84a0cd6d2a3d1
Quant11111 <lamagnere.quentin@gmail.com>
2024-08-27 13:30:24 +0200

    back to original config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const { withPlausibleProxy } = require("next-plausible");
-const fs = require("fs");
-const path = require("path");
 
-const isProd = process.env.PROJECT_ENV === "production";
-
-const nextConfig = withPlausibleProxy()({
-  server: isProd
-    ? {
-        https: {
-          key: fs.readFileSync(
-            path.resolve(
-              __dirname,
-              "/etc/letsencrypt/live/padelu.orbweavers.com/privkey.pem",
-            ),
-          ),
-          cert: fs.readFileSync(
-            path.resolve(
-              __dirname,
-              "/etc/letsencrypt/live/padelu.orbweavers.com/fullchain.pem",
-            ),
-          ),
-        },
-      }
-    : {},
-});
+const nextConfig = withPlausibleProxy()({});
 
 module.exports = nextConfig;
