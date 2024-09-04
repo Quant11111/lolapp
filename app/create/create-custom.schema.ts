4e55a534fa18502cf4d466288d7467f6515fce56
Quant11111 <lamagnere.quentin@gmail.com>
2024-09-05 01:42:40 +0200

    regex discord

--- app/create/create-custom.schema.ts
+++ app/create/create-custom.schema.ts
@@ -3,7 +3,13 @@ import { z } from "zod";
 export const CustomFormSchema = z.object({
   name: z.string().min(1, "Name is required"),
   description: z.string().min(1, "Description is required"),
-  discordLink: z.string().url().optional(),
+  discordLink: z
+    .string()
+    .regex(
+      /^(https?:\/\/)?(www\.)?(discord\.gg|discord\.com\/invite)\/[a-zA-Z0-9]+$/,
+      "Invalid Discord link",
+    )
+    .optional(),
 });
 
 export type CustomFormType = z.infer<typeof CustomFormSchema>;
