6fb2bc41ad0bef8556dd020186f2b06769964841
Quant11111 <lamagnere.quentin@gmail.com>
2024-09-06 13:01:52 +0200

    better card style

--- app/customs/page.tsx
+++ app/customs/page.tsx
@@ -21,36 +21,33 @@ export default async function HomePage() {
         customs.data.map((custom) => (
           <div
             key={custom.id}
-            className="relative flex h-1/3 w-80 flex-col items-center justify-center overflow-hidden rounded-lg outline outline-accent"
+            className="relative flex h-1/3 w-80 flex-col items-center justify-center overflow-hidden rounded-lg outline outline-1 outline-accent"
           >
-            <h1 className=" flex h-1/6 w-full items-center justify-center border-accent bg-primary-foreground/50 font-bold text-foreground outline outline-1 outline-accent">
+            <h1 className=" flex h-1/6 w-full items-center justify-center border-accent bg-accent font-bold text-primary-foreground ">
               {custom.name}
             </h1>
-            <div className="flex h-1/6 w-full items-center justify-center border-accent outline outline-1 outline-accent">
-              <a
-                href={`/summoners/${custom.creatorId}`}
-                className="no-style-link w-full scale-x-125 "
-              >
-                <Button
-                  variant={"default"}
-                  className="size-full outline outline-primary-foreground"
-                >
-                  {nameMap.get(custom.creatorId)}
-                </Button>{" "}
-              </a>
-            </div>
-            <p className="text-s flex h-1/2  w-full overflow-hidden border-accent px-2 py-1 text-justify outline outline-1 outline-accent">
+
+            <a
+              href={`/summoners/${custom.creatorId}`}
+              className="no-style-link flex h-10 w-full  items-center justify-center bg-primary-foreground/50 outline outline-1 outline-accent  hover:outline-foreground"
+            >
+              {nameMap.get(custom.creatorId)}
+            </a>
+
+            <p className="text-s flex w-full grow overflow-hidden border-accent px-2 py-1 text-justify ">
               {custom.description}
             </p>
-            <div className="flex h-1/6 w-full items-center justify-center overflow-hidden border-accent outline outline-1 outline-accent">
-              <a href={`/customs/${custom.id}`} className="w-full scale-x-125">
-                <Button variant={"outline"} className="size-full font-bold ">
-                  <Swords />
-                  <ArrowRight />
-                  <DoorOpen />
-                </Button>{" "}
-              </a>
-            </div>
+
+            <a
+              href={`/customs/${custom.id}`}
+              className="h-1/6 w-full scale-x-105"
+            >
+              <Button variant={"outline"} className="size-full font-bold ">
+                <Swords />
+                <ArrowRight />
+                <DoorOpen />
+              </Button>{" "}
+            </a>
           </div>
         ))}
     </div>
