32772270b651431a916907e0f06aa743bfef2b7b
Quant11111 <lamagnere.quentin@gmail.com>
2024-09-06 11:34:34 +0200

    insert try catch to handle err

--- app/customs/get-customs-action.ts
+++ app/customs/get-customs-action.ts
@@ -4,11 +4,15 @@ import { action } from "@/lib/backend/safe-actions";
 import { prisma } from "@/lib/prisma";
 
 export const getCustomAction = action.action(async () => {
-  const customs = await prisma.custom.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-    take: 50,
-  });
-  return customs;
+  try {
+    const customs = await prisma.custom.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 50,
+    });
+    return customs;
+  } catch (error) {
+    throw new Error("Error fetching customs");
+  }
 });
