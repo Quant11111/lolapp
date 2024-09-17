f79d9e0ec90046358c6099591a41f582205718a6
Quant11111 <lamagnere.quentin@gmail.com>
2024-09-17 19:44:51 +0200

    fix presentation aciton

--- app/summoners/[localUserId]/set-presentation.action.ts
+++ app/summoners/[localUserId]/set-presentation.action.ts
@@ -8,24 +8,14 @@ import { SetPresentationSchema } from "./set-presentation.schema";
 export const setPresentationAction = authAction
   .schema(SetPresentationSchema)
   .action(async ({ parsedInput: input, ctx }) => {
-    const summoner = await prisma.summoner.findUnique({
+    const user = await prisma.user.update({
       where: {
-        userId: ctx.user.id,
-      },
-    });
-    if (!summoner) {
-      throw new Error(
-        "You need to link your summoner account first to create a custom event",
-      );
-    }
-    const custom = await prisma.summoner.update({
-      where: {
-        userId: ctx.user.id,
+        id: ctx.user.id,
       },
       data: {
         pinnedPresentation: input.presentation,
       },
     });
 
-    return custom;
+    return user;
   });
